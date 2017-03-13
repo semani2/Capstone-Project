ca9b6661ca3d6531979c022d1301e37d917c9d73
Sai Prajnan Emani <sai.emani@citrix.com>
2017-03-13 10:03:52 -0400

    Collapsing toolbar layout implemented.

--- Travelogue/app/src/main/java/sai/developement/travelogue/activities/ViewTripActivity.java
+++ Travelogue/app/src/main/java/sai/developement/travelogue/activities/ViewTripActivity.java
@@ -1,15 +1,25 @@
 package sai.developement.travelogue.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
@@ -35,6 +45,18 @@ public class ViewTripActivity extends TravelogueActivity {
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
 
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+
+    /*@BindView(R.id.trip_name_text_view)
+    TextView tripNameTextView;
+
+    @BindView(R.id.trip_loc_text_view)
+    TextView tripLocTextView;*/
+
+    @BindView(R.id.toolbar_image_view)
+    ImageView tripImageView;
+
     private Trip mTrip = null;
 
     @Override
@@ -58,6 +80,36 @@ public class ViewTripActivity extends TravelogueActivity {
         mViewPager.setAdapter(mTripPagesAdapter);
 
         mTabLayout.setupWithViewPager(mViewPager);
+
+        mCollapsingToolbarLayout.setTitleEnabled(false);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(mTrip.getName());
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        /*tripNameTextView.setText(mTrip.getName());
+        tripLocTextView.setText(mTrip.getPrimaryLocation());*/
+
+        Glide.with(this)
+                .load(mTrip.getPhotoUrl())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model,
+                                                   Target<GlideDrawable> target,
+                                                   boolean isFromMemoryCache, boolean isFirstResource) {
+                        Bitmap bitmap = ((GlideBitmapDrawable) resource.getCurrent()).getBitmap();
+                        changeToolbarColors(bitmap);
+                        return false;
+                    }
+                })
+                .into(tripImageView);
     }
 
     @Override
@@ -92,6 +144,21 @@ public class ViewTripActivity extends TravelogueActivity {
         chatIntent.putExtras(extras);
         startActivity(chatIntent);
     }
+
+    private void changeToolbarColors(Bitmap bitmap) {
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            public void onGenerated(Palette palette) {
+                int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.colorPrimary));
+                int vibrantDarkColor = palette.getDarkVibrantColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                if (mCollapsingToolbarLayout != null) {
+                    mCollapsingToolbarLayout.setContentScrimColor(vibrantColor);
+                    mCollapsingToolbarLayout.setStatusBarScrimColor(vibrantDarkColor);
+                }
+            }
+        });
+    }
+
 
     @Override
     protected IEventHandler createEventHandler() {
